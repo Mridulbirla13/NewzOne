cb3ab1478da938b43dbbd110ac7477213e9429cc
Mridulbirla13 <mridulbirla13@gmail.com>
2024-05-30 11:59:57 +0530

    updated News.js

--- src/components/News.js
+++ src/components/News.js
@@ -56,41 +56,11 @@ export default class News extends Component {
     }
 
     handlePreviousClick=async ()=>{
-      // console.log('Previous');
-      // let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=680640962660406984c0e66da05133a9&page=${this.state.page-1}&pageSize=${this.props.pageSize}`;
-      // this.setState({
-      //   loading:true
-      // })
-      // let data = await fetch(url);
-      // let parsedData = await data.json()
-      // console.log(parsedData);
-      
-      // this.setState({
-      //   articles: parsedData.articles,
-      //   page:this.state.page - 1,
-      //   loading:false
-      // })
       this.setState({page: this.state.page -1})
       this.updateNews();
     }
 
     handleNextClick=async ()=>{
-      // console.log('Next');
-      // if(!(this.state.page+1>Math.ceil(this.state.totalResults/this.props.pageSize))){
-      //   let url = `https://newsapi.org/v2/top-headlines?country=${this.props.country}&category=${this.props.category}&apiKey=680640962660406984c0e66da05133a9&page=${this.state.page+1}&pageSize=${this.props.pageSize}`;
-      //   this.setState({
-      //     loading:true
-      //   })
-      //   let data = await fetch(url);
-      //   let parsedData = await data.json()
-      //   console.log(parsedData);
-        
-      //   this.setState({
-      //     articles: parsedData.articles,
-      //     page:this.state.page + 1,
-      //     loading:false
-      //   })
-      // }
       this.setState({page: this.state.page +1})
       this.updateNews();
     }
